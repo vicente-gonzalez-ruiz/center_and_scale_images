c16afa0d70873b3832ac0fdcb33eed92c935f4cf
Vicente González-Ruiz <vruiz@ual.es>
2023-10-13 14:32:59 +0200

    .

--- src/center_and_scale_images/__init__.py
+++ src/center_and_scale_images/__init__.py
@@ -27,6 +27,6 @@ def center_image(img, output_width=100, output_height=100, padding=10):
   padding_left = (output_width - new_width) // 2
   padded_img[padding_top:padding_top+new_height, padding_left:padding_left+new_width] = resized_img
 
-  info = f"center_and_scale {height} {width} {new_height} {new_width} {padding} # original_height original_width new_height new_width padding"
+  info = f"center_and_scale\t{height} {width} {new_height} {new_width} {padding} # original_height original_width new_height new_width padding"
   
   return padded_img, info
